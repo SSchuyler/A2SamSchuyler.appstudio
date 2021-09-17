d54532f9219c66ff3a0e74b5e946ac4657b58d55
Samuel Schuyler <sjs08496@creighton.edu>
2021-09-17 17:08:03 -0500

    added desc

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -21,25 +21,31 @@ FL	between 32 and 50 degrees	wear your warmest coat, a warm hat, a scarf, and wa
 FL	between 50 and 70	wear a warm coat, hat and gloves. Maybe a scarf too.
 */
 
-
+//setting up my variables
 let name = prompt('What is your name?')
 let state = prompt('Do you live in NE or FL?')
 let temp = prompt('What is the temperature outside in Fahrenheit?')
 
+//getting the array with different responses ready
 let message = ['wear a warm coat, hat, scarf and gloves.', 'wear a warm coat but you will not need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
 
+//testing to see if the first instance is true
 if (state == "NE" && temp < 32){
   console.log(`${name}, ${message[0]}`)
   }
+//testing to see if the second instance is true
 else if (state == "NE" && temp <= 50 && temp >= 32){
   console.log(`${name}, ${message[1]}`)
 }
+//testing to see if the third instance is true
 else if (state == "FL" && temp >= 32 && temp <= 50){
   console.log(`${name}, ${message[2]}`)
 }
+//testing to see if the fourth instance is true
 else if (state == "FL" && temp <= 70){
   console.log(`${name}, ${message[3]}`)
 }
+//if none of the instances are true
 else{
   console.log('You typed something in wrong, try again')
 }
